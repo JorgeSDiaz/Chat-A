ef933475c5dd8ae700f64b03bcdaad1cf809d42a
Jorge Saenz <saenzdiazj0@gmail.com>
2024-02-13 21:41:03 -0500

    Add input date: max date, current date

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -17,10 +17,20 @@ export default function Register() {
     watch,
   } = useForm();
 
+  const getCurrentDate = (): string => {
+    const date: Date = new Date();
+    const year: number = date.getFullYear();
+    const month: number = date.getMonth() + 1;
+    const day: number = date.getDate();
+
+    return `${year}-${month < 10 ? "0" + month.toString() : month}-${day < 10 ? "0" + day.toString() : day}`
+  }
+
   const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues) => {
     console.log(data);
   }
 
+  const inputStyle: string = "block w-full bg-gray-100 placeholder:text-zinc-500 rounded-md p-3 border ";
   const errorInputStyle: string = "border-red-500 focus:outline-red-500";
 
   const emailRules: string[] = ["Use of lowercase letters only", "Minimum one @ and one ."];
@@ -28,73 +38,77 @@ export default function Register() {
   const passwordRules: string[] = ["Use minimum one special characters", "Minimum 8 characters", "Use minimum one lowercase letter", "Use minimum one uppercase letter", "Use minimum one number"]
 
   return (
-    <div onSubmit={handleSubmit((data) => onSubmit(data))} className="bg-green-100 h-screen flex items-center">
-      <form className="w-72 mx-auto">
-        <h1 className="block text-4xl mb-6 text-sky-950 font-bold">Chat-A Register</h1>
-        <div className="flex flex-col gap-3">
-          <div>
-            <input
-              type="email"
-              placeholder="Email ⁕"
-              className={"block w-full rounded-md p-2 border " + `${errors.email ? errorInputStyle : ""}`}
-              {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/ })}
-            />
-            {errors.email?.type === "pattern" &&
-              <ErrorRulesMessage title="Invalid email" rules={emailRules} />
-            }
-          </div>
+    <div onSubmit={handleSubmit((data) => onSubmit(data))} className="bg-green-100 h-screen flex items-center justify-center">
+      <div className="bg-white p-12 rounded-lg">
+        <form className="w-72 mx-auto">
+          <h1 className="block text-4xl mb-8 text-sky-950 font-bold">Chat-A Register</h1>
+          <div className="flex flex-col gap-4">
+            <div>
+              <input
+                type="email"
+                placeholder="Email ⁕"
+                className={`${inputStyle}` + `${errors.email ? errorInputStyle : ""}`}
+                {...register("email", { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/ })}
+              />
+              {errors.email?.type === "pattern" &&
+                <ErrorRulesMessage title="Invalid email" rules={emailRules} />
+              }
+            </div>
 
-          <div>
-            <input
-              type="text"
-              placeholder="Username ⁕"
-              className={"block w-full rounded-md p-2 border " + `${errors.username ? errorInputStyle : ""}`}
-              {...register("username", { required: true, minLength: 5, maxLength: 20, pattern: /^[a-zA-Z0-9]+$/ })}
-            />
-            {(errors.username?.type === "minLength" || errors.username?.type === "maxLength" || errors.username?.type === "pattern") &&
-              <ErrorRulesMessage title="Invalid username" rules={usernameRules} />
-            }
-          </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Username ⁕"
+                className={`${inputStyle}` + `${errors.username ? errorInputStyle : ""}`}
+                {...register("username", { required: true, minLength: 5, maxLength: 20, pattern: /^[a-zA-Z0-9]+$/ })}
+              />
+              {(errors.username?.type === "minLength" || errors.username?.type === "maxLength" || errors.username?.type === "pattern") &&
+                <ErrorRulesMessage title="Invalid username" rules={usernameRules} />
+              }
+            </div>
 
-          <div>
-            <input
-              type="date"
-              placeholder="Date of Birth"
-              className={"block w-full rounded-md p-2 text-gray-400 border " + `${errors.birthDate ? errorInputStyle : ""}`}
-              {...register("birthDate", { required: true })}
-            />
-          </div>
+            <div>
+              <input
+                type="date"
+                placeholder="Date of Birth"
+                max={getCurrentDate()}
+                className={`${inputStyle}` + `${errors.birthDate ? errorInputStyle : ""}`}
+                {...register("birthDate", { required: true })}
+              />
+            </div>
 
-          <div>
-            <input
-              type="password"
-              placeholder="Password ⁕"
-              className={"block w-full rounded-md p-2 border " + `${errors.password ? errorInputStyle : ""}`}
-              {...register("password", { required: true, minLength: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]).{8,}$/ })}
-            />
-            {
-              (errors.password?.type === "minLength" || errors.password?.type === "pattern") &&
-              <ErrorRulesMessage title="Invalid password" rules={passwordRules} />
-            }
-          </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password ⁕"
+                className={`${inputStyle}` + `${errors.password ? errorInputStyle : ""}`}
+                max={(new Date().toISOString().split('T')[0]).toString()}
+                {...register("password", { required: true, minLength: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~\\-]).{8,}$/ })}
+              />
+              {
+                (errors.password?.type === "minLength" || errors.password?.type === "pattern") &&
+                <ErrorRulesMessage title="Invalid password" rules={passwordRules} />
+              }
+            </div>
 
-          <div>
+            <div>
 
-            <input
-              type="password"
-              placeholder="Confirm password ⁕"
-              className={"block w-full rounded-md p-2 border " + `${errors.confirmPassword ? errorInputStyle : ""}`}
-              {...register("confirmPassword", { required: true, validate: (value) => value === watch("password") })}
-            />
-            {errors.confirmPassword?.type === "validate" &&
-              <p className="text-xs text-red-500">Passwords do not match</p>
-            }
+              <input
+                type="password"
+                placeholder="Confirm password ⁕"
+                className={`${inputStyle}` + `${errors.confirmPassword ? errorInputStyle : ""}`}
+                {...register("confirmPassword", { required: true, validate: (value) => value === watch("password") })}
+              />
+              {errors.confirmPassword?.type === "validate" &&
+                <p className="text-xs text-red-500 font-bold">Passwords do not match</p>
+              }
+            </div>
           </div>
-        </div>
-        <button className="bg-green-500 text-white font-bold block w-full rounded-md p-2 mt-6 disabled:bg-green-300" disabled={Object.keys(errors).length > 0}>
-          Register
-        </button>
-      </form>
+          <button className="bg-green-500 text-white font-bold block w-full rounded-md p-2 mt-8 disabled:bg-green-300" disabled={Object.keys(errors).length > 0}>
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
